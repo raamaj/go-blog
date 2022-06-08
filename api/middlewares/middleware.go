093a58e70fcaa191bd6fb2931f3835930586e643
Rama Jayapermana <jayapermanarama@gmail.com>
2022-06-08 15:00:12 +0700

    Latihan Golang

---
+++ api/middlewares/middleware.go
@@ -0,0 +1,31 @@
+package middlewares
+
+import (
+	"errors"
+	"net/http"
+
+	"github.com/raamaj/go-blog/api/auth"
+	"github.com/raamaj/go-blog/api/responses"
+)
+
+func SetMiddlewareJSON(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		err := auth.TokenValid(r)
+		if err != nil {
+			responses.ERROR(w, http.StatusUnauthorized, errors.New("Unauthorized"))
+			return
+		}
+		next(w, r)
+	}
+}
+
+func SetMiddlewareAuthentication(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		err := auth.TokenValid(r)
+		if err != nil {
+			responses.ERROR(w, http.StatusUnauthorized, errors.New("Unauthorized"))
+			return
+		}
+		next(w, r)
+	}
+}
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/raamaj/go-blog/api"
+
+func main() {
+	api.Run()
+}
